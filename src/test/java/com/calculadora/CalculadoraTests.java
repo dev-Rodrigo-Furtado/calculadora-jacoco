dec9b365a815d719d4b34c58d882e531ea7d7ddd
Rodrigo Furtado <rodrigofurtado.8.8@gmail.com>
2023-02-12 17:03:17 -0300

    Casos de testes adicionados

--- src/test/java/com/calculadora/CalculadoraTests.java
+++ src/test/java/com/calculadora/CalculadoraTests.java
@@ -1,7 +1,44 @@
 package com.calculadora;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class CalculadoraTests {
 
 
+    public void deveRetornarAreaDoTriangulo() throws LadosInvalidosException {
+
+    }
+
+
+    public void deveLancarLadosInvalidosException_quandoLadosNaoFormamUmTriangulo() {
+
+    }
+
+
+    public void deveRetornarSemiperimetro() {
+
+    }
+
+
+    public void deveRetornarVerdadeiro_quandoLadosFormamUmTriangulo() {
+
+    }
+
+
+    public void deveRetornarFalso_quandoLadoAMaiorOuIgualSomaDosDemaisLados() {
+
+    }
+
+
+    public void deveRetornarFalso_quandoLadoBMaiorOuIgualSomaDosDemaisLados() {
+
+    }
+
+
+    public void deveRetornarFalso_quandoLadoCMaiorOuIgualSomaDosDemaisLados() {
+
+    }
 }
